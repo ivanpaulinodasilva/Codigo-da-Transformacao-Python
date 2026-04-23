864d3aa6636fa1bfca6002bfcf67a38383521e87
ivan-em-mobile <ivanemmobile@gmail.com>
2026-04-23 12:18:11 -0300

    create_repositorio

---
+++ modulo_00/sumario_modulo00.py
@@ -0,0 +1,10 @@
+'''
+Crie seu repositório: Inicie um repositório público no GitHub. Esse será o seu portfólio de projetos.
+
+Clone para o seu computador: Baixe o repositório para a sua máquina. É como ter uma cópia local do seu projeto para trabalhar offline.
+
+Codifique e compartilhe: Adicione seus arquivos, organize suas mudanças com commits e use o push para enviar tudo para o GitHub. Compartilhe seu progresso com a comunidade!
+
+Documente seu projeto: Crie um arquivo README.md detalhado. É o seu cartão de visitas e a forma de contar a história do seu código.
+
+'''
