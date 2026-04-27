5a4177dce9d3b38ff5dd290e32661ad2a33061a8
ivan-em-mobile <ivanemmobile@gmail.com>
2026-04-27 18:03:08 -0300

    reoganize_repositorio

---
+++ modulo_06/sumario_modulo06.py
@@ -0,0 +1,17 @@
+"""
+Crie e leia um arquivo `.txt`:** 
+Faça um programa para escrever e ler informações de 
+um arquivo de texto.
+
+Trabalhe com JSON:** 
+Salve e carregue um dicionário de clientes em um arquivo `.json`.
+
+Crie um sistema de notas em CSV:** 
+Desenvolva um programa para adicionar e salvar notas em 
+um arquivo `.csv` e depois carregá-las.
+
+Desafio Extra:** 
+Crie um sistema de backup automático com a biblioteca `shutil` 
+para copiar arquivos de uma pasta para outra.
+
+"""
